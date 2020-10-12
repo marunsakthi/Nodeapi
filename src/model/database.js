49cd3bc8427354e1f5eaa00d66fac73d436f06d0
marunsakthi <58545334+marunsakthi@users.noreply.github.com>
2020-10-12 18:03:36 +0530

    Update database.js

--- src/model/database.js
+++ src/model/database.js
@@ -3,7 +3,7 @@ var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   'content', // database
   'root', // user / usuario
-  'blaze.ws', //password
+  'root', //password
   {
     host: 'localhost',
     dialect: 'mysql'
